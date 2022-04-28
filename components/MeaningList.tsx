a390ec081c56708b93e7c7da71f3eb5ca21e6b2c
Prince Narteh <prinarteh@gmail.com>
2022-04-28 07:35:46 +0000

    adds prononciation

--- components/MeaningList.tsx
+++ components/MeaningList.tsx
@@ -22,10 +22,41 @@ const MeaningList = () => {
     <div className="max-w-5xl mx-auto">
       <div>
         <h1 className="text-4xl mt-5 font-semibold text-gray-600">
-          Hippopotamus
+          {word[0].word}{" "}
+          <small className="text-blueGreen text-2xl">{word[0].phonetic}</small>
         </h1>
-        <div className="text-xl text-blueGreen mt-2 font-medium">
-          <span>United Kingdom</span> | <span>\buk\</span>
+        <div className="text-xl text-blueGreen mt-2 font-medium flex items-center justify-center gap-2">
+          <span className="inline-flex items-center gap-2 ">
+            America
+            <audio src={`${word[0].phonetics[1].audio}`}></audio>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 cursor-pointer"
+              viewBox="0 0 20 20"
+              fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
+          |
+          <span className="inline-flex items-center gap-2 ">
+            United Kingdom
+            <audio src={`${word[0].phonetics[0].audio}`}></audio>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor">
+              <path
+                fillRule="evenodd"
+                d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </span>
         </div>
       </div>
 
